6ca0f8d5d9a99b356e731fd19f4ebf5d9c2f77fa
Ivan Grishaev <ivan@grishaev.me>
2016-09-10 18:09:54 +0300

    updated

--- 1-11.rkt
+++ 1-11.rkt
@@ -8,4 +8,10 @@
          (f-rec (- n 3)))))
 
 (define (tests)
-  (println (42)))
+  (println (= 1 (f-rec 1)))
+  (println (= 2 (f-rec 2)))
+  (println (= 3 (f-rec 3)))
+  (println (= 6 (f-rec 4)))
+  (println (= 11 (f-rec 5))))
+
+(tests)
